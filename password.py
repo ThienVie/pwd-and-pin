eb8d2f97b1aa5419d20384be660e82b86cb712c0
ThienViet <98856248+ThienVie@users.noreply.github.com>
2022-07-30 09:04:25 +0200

    Add files via upload

---
+++ password.py
@@ -0,0 +1,70 @@
+import secrets
+import string as s
+p = "pin"
+pss = "password"
+
+chg = s.ascii_uppercase
+chk = s.ascii_lowercase
+chzah = s.digits
+chzei = s.punctuation
+f = False
+
+#---------------------------
+
+print("")
+print(f"Options:    {p}    {pss}")
+print("")
+
+while f == False:
+    c = str(input("What is it: "))
+    if c == pss:
+        f = True
+    elif c == p:
+        f = True
+    else:
+        print("This is not an option")
+        print("")
+pwd = ""
+    
+
+if c == p:
+    ch = chzah
+    a = 4
+else:
+    ch = chg + chk + chzah + chzei
+    a = int(input("How long has it to be: "))
+
+for _ in range(a):
+    pwd += secrets.choice(ch)
+l = c
+
+if a <= 3:
+    print("")
+    print("----------------------")
+    print("")
+    print(f"Your {l} is: {pwd}")
+    print("")
+    print("Erm...")
+    print("Can you explain why you choose 1, 2, or 3?")
+    print("I really don't get it.")
+    print("but still I wish you a happy day. <3")
+    print("")
+elif a == 0:
+    print("")
+    print("----------------------")
+    print("")
+    print("This is really sad, that you choose zero, :(")
+    print("but still I wish you a happy day. <3")
+    print("If today is your birthday, then I wish you a happy birthday.")
+    print("")
+else:
+    print("")
+    print("----------------------")
+    print("")
+    print(f"Your {l} is: {pwd}")
+    print("")
+    print("----------------------")
+    print("")
+    print("I wish you a happy day. <3")
+    print("If today is your birthday, then I wish you a happy birthday.")
+    print("")
